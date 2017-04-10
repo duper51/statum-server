7e92d49c7c7278b775aa998417c3ecbc7200ddfe
duper51 <ianotto24@gmail.com>
2017-04-09 18:48:08 -0700

    Start for Services

--- index.php
+++ index.php
@@ -2,8 +2,5 @@
 require_once "vendor/autoload.php";
 
 $router = new \Klein\Klein();
-$router->respond(function() {
-    return "<h1> GG </h1>";
-});
 $kernel = new \Me\Kernel($router);
 $router->dispatch();
