c6a8faf6b0fbb009b6fdf206507f75fad2aa5f40
root <root@ip-172-31-1-17.us-west-1.compute.internal>
2017-04-10 09:22:03 +0000

    Setup PropelORM so that we can start using it

---
+++ lib/Models/Me/Models/Me/Models/Services.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Me\Models\Me\Models;
+
+use Me\Models\Me\Models\Base\Services as BaseServices;
+
+/**
+ * Skeleton subclass for representing a row from the 'services' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class Services extends BaseServices
+{
+
+}
---
+++ propel.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'propel' => [
+        'database' => [
+            'connections' => [
+                'default' => [
+                    'adapter' => 'mysql',
+                    'dsn' => 'mysql:host=shopquick.c6v6ihfez0ei.us-west-1.rds.amazonaws.com;port=3306;dbname=statum',
+                    'user' => 'root',
+                    'password' => 'ShopQuickMain',
+                    'settings' => [
+                        'charset' => 'utf8'
+                    ]
+                ]
+            ]
+        ]
+    ]
+];
